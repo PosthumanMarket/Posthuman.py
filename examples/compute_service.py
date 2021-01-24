eef3532be39821091f479edf9e421ad4312e8da1
dhruv <dhruv96@gmail.com>
2021-01-24 17:23:09 +0530

    indent

--- examples/compute_service.py
+++ examples/compute_service.py
@@ -168,7 +168,7 @@ algo_file = './examples/data/algorithm.py'
 #order_tx_id=
 order_tx_id=''
 job_id, status = run_compute(ocean, asset.did, consumer, algo_file, pool.address, order_tx_id)
-    print(f'Compute started on asset {asset.did}: job_id={job_id}, status={status}')
+print(f'Compute started on asset {asset.did}: job_id={job_id}, status={status}')
 
 
 if __name__ == '__main__':
