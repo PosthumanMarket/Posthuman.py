0607b02557f79762a93af0169879c5fb405c2b84
PosthumanMarket <treedmt@gmail.com>
2021-03-14 19:20:09 +0530

    sample infer text

--- examples/data/algo_inference.py
+++ examples/data/algo_inference.py
@@ -13,8 +13,10 @@ if inference_link:
 
 #read from inference file
 
-with open(inference_file) as f:
-    infer_text = f.read()
+#with open(inference_file) as f:
+    #infer_text = f.read()
+
+infer_text = "hello my name is "
 
 def complete_text(text):
     r = generator(text, max_length=60, num_return_sequences=5)
