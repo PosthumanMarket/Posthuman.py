a48c320f871bba9f98414d0311b3e852014e5096
dhruvluci <39732134+dhruvluci@users.noreply.github.com>
2021-05-31 23:21:55 +0530

    job funct
    get job

--- algorithims/algo_inference.py
+++ algorithims/algo_inference.py
@@ -1,6 +1,44 @@
 import subprocess
 import sys
 
+
+def get_job_details():
+    """Reads in metadata information about assets used by the algo"""
+    job = dict()
+    job['dids'] = json.loads(os.getenv('DIDS', None))
+    job['metadata'] = dict()
+    job['files'] = dict()
+    job['algo'] = dict()
+    job['secret'] = os.getenv('secret', None)
+    algo_did = os.getenv('TRANSFORMATION_DID', None)
+    if job['dids'] is not None:
+        for did in job['dids']:
+            # get the ddo from disk
+            filename = '/data/ddos/' + did
+            print(f'Reading json from {filename}')
+            with open(filename) as json_file:
+                ddo = json.load(json_file)
+                # search for metadata service
+                for service in ddo['service']:
+                    if service['type'] == 'metadata':
+                        job['files'][did] = list()
+                        index = 0
+                        for file in service['attributes']['main']['files']:
+                            job['files'][did].append(
+                                '/data/inputs/' + did + '/' + str(index))
+                            index = index + 1
+    if algo_did is not None:
+        job['algo']['did'] = algo_did
+        job['algo']['ddo_path'] = '/data/ddos/' + algo_did
+    return job
+
+
+print('Starting compute job with the following input information:')
+print(json.dumps(job_details, sort_keys=True, indent=4))
+
+""" Now, count the lines of the first file in first did """
+
+
 i=0
 def install():
     subprocess.check_call([sys.executable, "-m", "pip", "install", "transformers"])
@@ -19,9 +57,11 @@ import sys
 import os
 
 import zipfile
-path_to_zip_file = "/data/inputs/distilgpt2.zip"
+first_did = job_details['dids'][0]
+filename = job_details['files'][first_did][0]
+path_to_zip_file = filename
 with zipfile.ZipFile(path_to_zip_file, 'r') as zip_ref:
-    zip_ref.extractall("/data/inputs/distilgpt2")
+    zip_ref.extractall("/data/outputs/distilgpt2")
 
 generator = pipeline('text-generation', model='/data/inputs/distilgpt2')
 set_seed(42)
@@ -39,7 +79,6 @@ if inference_link:
 #with open(inference_file) as f:
     #infer_text = f.read()
 
-infer_text2 = "hello my name is "
 
 def complete_text(text):
     r = generator(text, max_length=200, num_return_sequences=5)
