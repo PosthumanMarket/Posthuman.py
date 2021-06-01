e0c8a6e83f270f31966bf27e312bf9375449da54
dhruvluci <39732134+dhruvluci@users.noreply.github.com>
2021-06-01 23:16:31 +0530

    Update algo_inference.py
    pytorch container to reduce time

--- algorithims/algo_inference.py
+++ algorithims/algo_inference.py
@@ -41,7 +41,6 @@ def get_job_details():
 
 i=0
 def install():
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "torch"])
     subprocess.check_call([sys.executable, "-m", "pip", "install", "transformers"])
     i=2
 
